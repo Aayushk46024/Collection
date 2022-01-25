6d2efa6287db52615e16891ae4908eeea2055bd0
AayushKhandelwal <aayush.khandelwal@infostride.com>
2022-01-25 13:38:39 +0530

    Initial Commit

---
+++ Arraylist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Arraylist {
+    public static void main(String args[]){
+        ArrayList<String> list=new ArrayList<String>();//Creating arraylist
+        list.add("Aayush");
+        list.add("Amit");
+        list.add("Saurav");
+        list.add("Sachin");
+
+        Iterator itr=list.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
